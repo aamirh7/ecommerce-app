33f2ac4c9912469575c75bd98fc99f46f1d3ebe3
aamirh7 <aamirhussainkot708@gmail.com>
2025-01-29 23:24:32 +0500

    first commit

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -12,6 +12,7 @@ const LatestCollection = () => {
         <div className='my-10'>
             <div className='text-center py-8 text-3xl'>
                 <Title text1={'LATEST'} text2={'COLLECTION'} />
+                <p className='w-3/4 m-auto text-xs md:text-base text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, laudantium voluptas? Numquam labore inventore reprehenderit. Aperiam magni sequi voluptates similique laborum neque voluptatum velit quaerat itaque est, corporis, excepturi quis!</p>
             </div>
         </div>
     )
