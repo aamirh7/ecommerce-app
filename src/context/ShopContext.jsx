912fc33e727821ad0deb73fcde30cac62ab9a684
aamirh7 <aamirhussainkot708@gmail.com>
2025-01-30 15:34:58 +0500

    Refactor: Update BestSeller component and clean up old files
    - Added new BestSeller component (BestSeller.jsx)
    - Removed old BestSellers.jsx component
    - Modified ShopContext and Home pages for updates

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -15,7 +15,7 @@ const ShopContextProvider = (props) => {
     }
     return (
         <ShopContext.Provider value={value}>
-            {props.children}
+            {props.Children}
         </ShopContext.Provider>
     )
 }
